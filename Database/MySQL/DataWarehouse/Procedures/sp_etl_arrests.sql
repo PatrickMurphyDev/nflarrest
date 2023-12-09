e11f5e04993e8aeef38a8758f5ca74c45e31fb54
Patrick Murphy <patrickmurphydev@gmail.com>
2023-12-09 09:42:13 -0800

    fix etl sproc

--- Database/MySQL/DataWarehouse/Procedures/sp_etl_arrests.sql
+++ Database/MySQL/DataWarehouse/Procedures/sp_etl_arrests.sql
@@ -1,6 +1,55 @@
 -- Active: 1701775194203@@127.0.0.1@3306@nflarrest
-DROP PROCEDURE sp_etl_arrests;
-CREATE DEFINER=`root`@`localhost` PROCEDURE `sp_etl_arrests`()
-BEGIN
+	--CREATE TEMPORARY TABLE StageData
+	
+/*
+	CREATE TEMPORARY
+	TABLE StageDataFailed
+	SELECT *
+	FROM StageData
+	WHERE (day_key IS NULL)
+	    OR (dim_player_position_id IS NULL)
+	    OR (dim_team_id IS NULL)
+        OR (dim_crime_id IS NULL);*/
 
-END
+DELIMITER //
+DROP PROCEDURE if EXISTS sp_etl_arrests;
+
+CREATE PROCEDURE `SP_ETL_ARRESTS`()
+ BEGIN
+	INSERT INTO
+	    fct_arrests (
+	        dim_date_id,
+	        dim_team_id,
+	        dim_player_position_id,
+	        dim_crime_id,
+	        `Name`,
+	        `Description`,
+	        `Encounter_raw`,
+	        `Crime_raw`,
+	        `Outcome_raw`
+	    )
+        SELECT
+	    dd.day_key,
+	    dt.dim_team_id,
+	    dpp.dim_player_position_id,
+	    if(dc.dim_crime_id is null, 27, dc.dim_crime_id) as dim_crime_id,
+	    stg.`Name`,
+	    stg.`Description`,
+	    stg.`Encounter`,
+	    stg.`Category`,
+	    stg.`Outcome`
+	FROM stg_arrests as stg
+	    LEFT JOIN dim_day as dd ON dd.`date` = stg.`Date`
+	    LEFT JOIN dim_player_position as dpp ON dpp.position_code = stg.`Position`
+	    LEFT JOIN dim_team as dt ON dt.team_code = stg.`Team`
+	    LEFT JOIN xw_category_to_crime as xwc ON xwc.`Category` = stg.`Category` AND xwc.record_count = (SELECT MAX(xwc2.record_count) FROM xw_category_to_crime as xwc2 WHERE xwc2.`Category` = stg.`Category`)
+	    LEFT JOIN dim_crime as dc ON xwc.dim_crime_id = dc.dim_crime_id
+    WHERE (dd.day_key IS NOT NULL)
+	    AND (dpp.dim_player_position_id IS NOT NULL)
+	    AND (dt.dim_team_id IS NOT NULL);
+
+	TRUNCATE TABLE stg_arrests;
+
+	END
+
+DELIMITER ;
---
+++ Database/MySQL/DataWarehouse/Tables/DefineStageStructure/nflarrest_stg_arrests_failed_2023-12-09_085621.sql
@@ -0,0 +1,18 @@
+/*!40101 SET NAMES utf8 */;
+/*!40014 SET FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET SQL_NOTES=0 */;
+DROP TABLE IF EXISTS stg_arrests_failed;
+CREATE TABLE `stg_arrests_failed` (
+  `stg_arrests_id` int NOT NULL AUTO_INCREMENT,
+  `Date` varchar(45) DEFAULT NULL,
+  `Team` varchar(11) DEFAULT NULL,
+  `Name` varchar(45) DEFAULT NULL,
+  `Position` varchar(5) DEFAULT NULL,
+  `Encounter` varchar(12) DEFAULT NULL,
+  `Category` varchar(25) DEFAULT NULL,
+  `Description` varchar(255) DEFAULT NULL,
+  `Outcome` varchar(255) DEFAULT NULL,
+  PRIMARY KEY (`stg_arrests_id`),
+  UNIQUE KEY `reqGrainStg` (`Date`,`Team`,`Name`,`Description`,`Category`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
